bf614474dfc4280a1ce12d293781b3066cac1ecb
Andrii Pukhalyk <pukhalyk1@gmail.com>
2026-01-27 18:46:58 -0700

    It now actually works
    It only works with one terrain set, but it still works!

--- dual_tile_map_layer.gd
+++ dual_tile_map_layer.gd
@@ -1,44 +1,97 @@
 class_name DualTileMapLayer extends TileMapLayer
 
+# currently this only supports 1 terrain set (0)
+## map that maps Array of 4 [int] to atlas tile position (Vector2i)
+## key: [0] = top left, [1] = top right, [2] = bottom left, [3] = bottom right
+## value: -1 = empty, 0 = grass
+var terrain_to_tile: Dictionary[Array, Vector2i] = {}
 
-# TODO: make used_cells a dictionary, where values are cell info (like what terrain it uses)
-# godot does not have sets yet, so pretend that this is a set (dictionary without values)
-## A set of all used cells (not dual cells that are displayed, there are real tile positions)
-var used_cells: Dictionary[Vector2i, Variant] = {}
+## which tile set atlas to use
+var source_id := 0
+var tile_set_index := 0
+
+## A dictionary that maps tile position (Vector2i) to tile type (int)
+var used_cells: Dictionary[Vector2i, int] = {}
 
 
+func _ready() -> void:
+	var tile_set_source: TileSetSource = tile_set.get_source(tile_set_index)
+	if tile_set_source is not TileSetAtlasSource:
+		push_error("Only TileSetAtlasSource is supported")
+	
+	var tile_source: TileSetAtlasSource = tile_set_source
+	var grid_size = tile_source.get_atlas_grid_size()
+	for x in grid_size.x:
+		for y in grid_size.y:
+			if tile_source.get_tile_at_coords(Vector2i(x, y)) == Vector2i(-1, -1):
+				continue
+
+			var tile_data: TileData = tile_source.get_tile_data(Vector2i(x, y), 0)
+			# you can also check if the cell neighbor is valid using tile_data.is_valid_terrain_peering_bit(...)
+			var key: Array[int] = [
+				tile_data.get_terrain_peering_bit(TileSet.CellNeighbor.CELL_NEIGHBOR_TOP_LEFT_CORNER),
+				tile_data.get_terrain_peering_bit(TileSet.CellNeighbor.CELL_NEIGHBOR_TOP_RIGHT_CORNER),
+				tile_data.get_terrain_peering_bit(TileSet.CellNeighbor.CELL_NEIGHBOR_BOTTOM_LEFT_CORNER),
+				tile_data.get_terrain_peering_bit(TileSet.CellNeighbor.CELL_NEIGHBOR_BOTTOM_RIGHT_CORNER),
+			]
+			terrain_to_tile[key] = Vector2i(x, y)
+	
+
+# func add_tile(pos: Vector2i, terrain_set: int = 0, terrain: int = 0, ignore_empty_terrains: bool = true) -> void:
 ## add a tile at [param pos]
-func add_tile(pos: Vector2i, terrain_set: int = 0, terrain: int = 0, ignore_empty_terrains: bool = true) -> void:
-	# add the cell to the used cells set
-	used_cells[pos] = null
+func add_tile(pos: Vector2i) -> void:
+	# add the cell to the used cells
+	used_cells[pos] = 0
 
-	# for p in pos_to_dual(pos):
-	# 	erase_cell(p)
-
-	# this only works with path mode, not connect mode
-	set_cells_terrain_path(pos_to_dual(pos), terrain_set, terrain, ignore_empty_terrains)
+	for dual_pos in pos_to_dual(pos):
+		set_cell(dual_pos, source_id, format_tile(dual_pos))
 
 
-func remove_tile(pos: Vector2i) -> void:
-	# remove the cell from the used cells set
+## remove the tile at [param pos]
+## if [param keep_empty] is true, this will NOT remove empty tiles (so it never removes anything from the dual grid, just updates it)
+func remove_tile(pos: Vector2i, keep_empty: bool = false) -> void:
 	used_cells.erase(pos)
 
-	# remove 4 corresponding cells from the dual tile map
-	for cell in pos_to_dual(pos):
-		erase_cell(cell)
+	# update the 4 corresponding dual tile map cells
+	for dual_pos in pos_to_dual(pos):
+		var formatted_tile: Vector2i = format_tile(dual_pos)
+		# if no one is around this tile, remove it
+		if not keep_empty and formatted_tile == terrain_to_tile[[-1, -1, -1, -1]]:
+			erase_cell(dual_pos)
+		# if there is someone near this tile (who is not current tile, since we removed it), just update it
+		else:
+			set_cell(dual_pos, source_id, format_tile(dual_pos))
 
-	# update the 8 cells around the removed cell
-	for cell in existing_surrounding_cells(pos, used_cells):
-		add_tile(cell)
+
+## takes a dual tile map position and returns the formatted atlas tile position
+func format_tile(dual_pos: Vector2i) -> Vector2i:
+	var key: Array[int] = []
+
+	# pos is in dual tile map, i format it based on 4 REAL tiles around it (so not dual grid tiles)
+	# so i kind of format it based on the tiles around it in different dimension
+	for pos in dual_to_pos(dual_pos):
+		key.append(used_cells.get(pos, -1))
+
+	return terrain_to_tile[key]
 
 
+## takes a tile position, returns 4 corner positions
 func pos_to_dual(pos: Vector2i) -> Array[Vector2i]:
 	# Note: pos is bottom right tile in the dual tile map
 	return [
-		pos - Vector2i(0, 0), # bottom right
-		pos - Vector2i(1, 0), # bottom left
-		pos - Vector2i(0, 1), # top right
 		pos - Vector2i(1, 1), # top left
+		pos - Vector2i(0, 1), # top right
+		pos - Vector2i(1, 0), # bottom left
+		pos - Vector2i(0, 0), # bottom right
+	]
+
+func dual_to_pos(dual_pos: Vector2i) -> Array[Vector2i]:
+	# Note: dual_pos is top left in the real tile map
+	return [
+		dual_pos + Vector2i(0, 0), # top left
+		dual_pos + Vector2i(1, 0), # top right
+		dual_pos + Vector2i(0, 1), # bottom left
+		dual_pos + Vector2i(1, 1), # bottom right
 	]
 
 
--- selector.gd
+++ selector.gd
@@ -1,19 +1,33 @@
-extends Sprite2D
+@tool
+extends Node2D
 
+## if [member keep_empty] is true, this will NOT remove empty tiles (so it never removes anything from the dual grid, just updates it)
+## see [method DualTileMapLayer.remove_tile]
+@export var keep_empty := false
 
 # @export var tilemap: TileMapLayer
 @export var dual_tile_map: DualTileMapLayer
+
+
+func _ready() -> void:
+	# only do _draw in the editor
+	if Engine.is_editor_hint():
+		set_process(false)
 
 
 func _process(_delta: float) -> void:
 	var mouse_pos = get_global_mouse_position()
 	var grid_pos: Vector2i = (mouse_pos / 16).round()
 	position = grid_pos * 16
+
 	if Input.is_action_pressed(&"add_tile"):
 		dual_tile_map.add_tile(grid_pos)
 	
 	if Input.is_action_pressed(&"remove_tile"):
-	# if Input.is_action_just_pressed(&"remove_tile"):
-		dual_tile_map.remove_tile(grid_pos)
-		# dual_tile_map.set_cell(grid_pos, 0, Vector2(0, 3))
-		# dual_tile_map.add_tile(grid_pos, 0, 0)
+		dual_tile_map.remove_tile(grid_pos, keep_empty)
+
+
+func _draw() -> void:
+	var sides = [Vector2(-8, -8), Vector2(8, -8), Vector2(8, 8), Vector2(-8, 8)]
+	for i in range(sides.size()):
+		draw_dashed_line(sides[i], sides[(i + 1) % sides.size()], Color.WHITE, 1, 6)
