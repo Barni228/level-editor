d5cce3f6dd2e76dc8b529e99fc19d0ba06c95ce6
Andrii Pukhalyk <pukhalyk1@gmail.com>
2026-02-06 19:40:16 -0700

    save tile maps as compressed binary
    now this will save maps as compressed binary (making file size ~16 times
    smaller) instead of Godot resource

--- dual_tile_map_data.gd
+++ dual_tile_map_data.gd
@@ -45,3 +45,49 @@ func has(pos: Vector2i) -> bool:
 ## get all tile positions
 func keys() -> Array[Vector2i]:
 	return tile_map.keys()
+
+
+## This will return a compressed tile map bytes
+## it is pretty slow (because of compression), so it is recommended to use only use when saving to a file
+func to_bytes() -> PackedByteArray:
+	# it stores stuff like this:
+	# [u16 pos.x, u16 pos.y, u8 tile_id, ...]
+	# then compressed using ZSTD
+	# then last 4 bytes is the size of uncompressed bytes (original size)
+	var bytes = PackedByteArray()
+	var i = 0
+	bytes.resize(tile_map.size() * 5)
+
+	for pos in tile_map.keys():
+		bytes.encode_s16(i, pos.x)
+		i += 2
+		bytes.encode_s16(i, pos.y)
+		i += 2
+		bytes.encode_u8(i, tile_map[pos] + 1)  # make -1 a valid u8
+		i += 1
+
+	# Z standard compression, it results in the smallest size, but slow
+	# the default (FASTLZ) is fast, but does not compress that well
+	var compressed = bytes.compress(FileAccess.COMPRESSION_ZSTD)
+	compressed.resize(compressed.size() + 4)
+	# append the real size of uncompressed bytes at the end, so i can easily remove it later
+	compressed.encode_u32(compressed.size() - 4, bytes.size())
+	return compressed
+
+
+func from_bytes(compressed_bytes: PackedByteArray) -> void:
+	# get the original size
+	var size = compressed_bytes.decode_u32(compressed_bytes.size() - 4)
+	# remove last 4 bytes that represented the size (now everything is just valid compressed bytes)
+	compressed_bytes.resize(compressed_bytes.size() - 4)
+	var bytes = compressed_bytes.decompress(size, FileAccess.COMPRESSION_ZSTD)
+	tile_map = {}
+	var i = 0
+	while i < bytes.size():
+		var x = bytes.decode_s16(i)
+		i += 2
+		var y = bytes.decode_s16(i)
+		i += 2
+		var tile = bytes.decode_u8(i) - 1  # convert tile ID back to -1..=254
+		i += 1
+		tile_map[Vector2i(x, y)] = tile
--- save_or_load.gd
+++ save_or_load.gd
@@ -1,20 +1,17 @@
 extends VBoxContainer
 
 
-# TODO: use json, or something like that
-
-
 @export var dual_tile_map: DualTileMapLayer
 @export_file("*.tres") var save_path: String
 
 func save_data() -> void:
 	print("Saving")
 	var data := dual_tile_map.data.duplicate(true)
-	# data.resource_path = save_path
-	ResourceSaver.save(data, save_path)
+	var file := FileAccess.open(save_path, FileAccess.WRITE)
+	file.store_buffer(data.to_bytes())
 
 func load_data() -> void:
 	print("Loading")
-	# dual_tile_map.data = load(save_path)
-	dual_tile_map.data = ResourceLoader.load(save_path, &"DualTileMapData", ResourceLoader.CACHE_MODE_IGNORE)
+	var bytes := FileAccess.get_file_as_bytes(save_path)
+	dual_tile_map.data.from_bytes(bytes)
 	dual_tile_map.update_every_tile()
